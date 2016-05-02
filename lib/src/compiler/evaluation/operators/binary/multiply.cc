66d5adaf17085c2eff55af79e17f5a8eb0c8693a
Peter Huene <peter.huene@puppet.com>
2016-05-02 14:14:30 -0700

    (NPUP-10) Implement String to Numeric type coercion for arithmetic expressions.
    Implementing type coercion from String to Numeric for arithmetic expressions.
    
    String operands for operators `+`, `-` (binary), `*` (binary), and `/` are now
    coerced to a numeric value.  If the string cannot be coerced, an evaluation
    exception is thrown.

--- lib/src/compiler/evaluation/operators/binary/multiply.cc
+++ lib/src/compiler/evaluation/operators/binary/multiply.cc
@@ -6,11 +6,11 @@
 #include <cfenv>
 
 using namespace std;
-using namespace puppet::runtime;
+using namespace puppet::runtime::values;
 
 namespace puppet { namespace compiler { namespace evaluation { namespace operators { namespace binary {
 
-    static values::value multiply_by(call_context& context, int64_t left, int64_t right)
+    static value multiply_by(call_context& context, int64_t left, int64_t right)
     {
         auto& evaluation_context = context.context();
 
@@ -56,7 +56,7 @@ namespace puppet { namespace compiler { namespace evaluation { namespace operato
         return left * right;
     }
 
-    static values::value multiply_by(call_context& context, double left, double right)
+    static value multiply_by(call_context& context, double left, double right)
     {
         auto& evaluation_context = context.context();
 
@@ -84,21 +84,38 @@ namespace puppet { namespace compiler { namespace evaluation { namespace operato
         return result;
     }
 
+    static value multiply_by(call_context& context, value const& left, value const& right)
+    {
+        if (left.as<int64_t>() && right.as<int64_t>()) {
+            return multiply_by(context, left.require<int64_t>(), right.require<int64_t>());
+        }
+        if (left.as<int64_t>()) {
+            return multiply_by(context, static_cast<double>(left.require<int64_t>()), right.require<double>());
+        }
+        if (right.as<int64_t>()) {
+            return multiply_by(context, left.require<double>(), static_cast<double>(right.require<int64_t>()));
+        }
+        return multiply_by(context, left.require<double>(), right.require<double>());
+    }
+
+    // Declared in divide.cc
+    value arithmetic_string_conversion(call_context& context, bool left = true);
+
     descriptor multiply::create_descriptor()
     {
         binary::descriptor descriptor{ ast::binary_operator::multiply };
 
-        descriptor.add("Integer", "Integer", [](call_context& context) {
-            return multiply_by(context, context.left().require<int64_t>(), context.right().require<int64_t>());
+        descriptor.add("Numeric", "Numeric", [](call_context& context) {
+            return multiply_by(context, context.left(), context.right());
         });
-        descriptor.add("Integer", "Float", [](call_context& context) {
-            return multiply_by(context, static_cast<double>(context.left().require<int64_t>()), context.right().require<double>());
+        descriptor.add("String", "Numeric", [](call_context& context) {
+            return multiply_by(context, arithmetic_string_conversion(context), context.right());
         });
-        descriptor.add("Float", "Integer", [](call_context& context) {
-            return multiply_by(context, context.left().require<double>(), static_cast<double>(context.right().require<int64_t>()));
+        descriptor.add("Numeric", "String", [](call_context& context) {
+            return multiply_by(context, context.left(), arithmetic_string_conversion(context, false));
         });
-        descriptor.add("Float", "Float", [](call_context& context) {
-            return multiply_by(context, context.left().require<double>(), context.right().require<double>());
+        descriptor.add("String", "String", [](call_context& context) {
+            return multiply_by(context, arithmetic_string_conversion(context), arithmetic_string_conversion(context, false));
         });
         return descriptor;
     }
---
+++ lib/tests/fixtures/compiler/evaluation/plus_bad_left_string.pp
@@ -0,0 +1,1 @@
+notice('foo' + 1)
